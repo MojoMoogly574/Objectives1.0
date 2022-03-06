a86c8639d76960e17ecd32141d6833dce71f117f
Mojo <mojo@nuralab.com>
2022-03-06 15:19:57 -0600

    Initial Commit

---
+++ Objectives1.0/Objectives1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Objectives1_0App.swift
+//  Objectives1.0
+//
+//  Created by Joseph DeWeese on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Objectives1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
